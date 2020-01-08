d01b3a5bcdb169d01efc7afce0283aa5640ad219
David Paquette <dave@davepaquette.com>
2020-01-07 19:30:37 -0700

    Cloud Role Name Telemetry Initializer

---
+++ AspNetMonsters.ApplicationInsights/AspNetMonsters.ApplicationInsights/TelemetryInitializers/CloudRoleNameTelemetryInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace AspNetMonsters.ApplicationInsights.TelemetryInitializers
+{
+    public class CloudRoleNameTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly string _cloudRoleName;
+
+        public CloudRoleNameTelemetryInitializer(string cloudRoleName)
+        {
+            _cloudRoleName = cloudRoleName;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = _cloudRoleName;   
+        }
+    }
+}
