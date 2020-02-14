2508e279068b1d1cfd8bc031421d30f0ad5c361f
David Paquette <dave@davepaquette.com>
2020-02-14 08:01:35 -0700

    Add tests

---
+++ AspNetMonsters.ApplicationInsights/AspNetMonsters.ApplicationInsights.Tests/CloudRoleNameTelementeryInitializerTests.cs
@@ -0,0 +1,25 @@
+﻿using AspNetMonsters.ApplicationInsights.TelemetryInitializers;
+using Microsoft.ApplicationInsights.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AspNetMonsters.ApplicationInsights.Tests
+{
+    public class CloudRoleNameTelementeryInitializerTests
+    {
+        [Fact]
+        public void ShouldSetCloudRoleNameOnTelemetry()
+        {
+            var cloudRoleName = "MyFancyApp";
+            var sut = new CloudRoleNameTelemetryInitializer(cloudRoleName);
+
+            var telemetry = new RequestTelemetry();
+
+            sut.Initialize(telemetry);
+
+            Assert.Equal(cloudRoleName, telemetry.Context.Cloud.RoleName);
+        }
+    }
+}
